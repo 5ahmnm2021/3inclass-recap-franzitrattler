548ec245426188dc4bfe3d5838d38c76c81ae5fc
franzitrattler <franziska.trattler@gmail.com>
2021-03-16 10:23:25 +0100

    updated Scrips

--- Assets/MyGame/Scripts/Additon.cs
+++ Assets/MyGame/Scripts/Additon.cs
@@ -13,36 +13,45 @@ public class Additon : MonoBehaviour
     public float Zahl1float;
     public float Zahl2float;
 
+    public bool bool1 = true;
+    public bool bool2 = true; 
+
 
     public void Addition()
     {
         try
         {
             Zahl1float = float.Parse(Zahl1.text);
-            Zahl1.image.color = new Color32(255, 255, 255, 255); 
+            Zahl1.image.color = new Color32(255, 255, 255, 255);
+            bool1 = true;
         }
         catch (System.Exception)
         {
             Zahl1.image.color = new Color32(255, 0, 0, 255);
-            Zahl1.text = "Enter a valid number"; 
+            Zahl1.text = "Enter a valid number";
+            bool1 = false; 
         }
         try
         {
             Zahl2float = float.Parse(Zahl2.text);
             Zahl2.image.color = new Color32(255, 255, 255, 255);
+            bool2 = true;
         }
         catch (System.Exception)
         {
             Zahl2.image.color = new Color32(255, 0, 0, 255);
             Zahl2.text = "Enter a valid number";
+            bool2 = false;
         }
        
-
-        //Additon
-      //  int if1Int = int.Parse(Zahl1.text);
-      //  int if2Int = int.Parse(Zahl2.text);
-        
-        float ergebnisInt = Zahl1float + Zahl2float;
-        ergebnis.text = ergebnisInt.ToString();
+        if(bool1 == true && bool2 == true)
+        {
+            float ergebnisInt = Zahl1float + Zahl2float;
+            ergebnis.text = ergebnisInt.ToString();
+        }
+        else
+        {
+            ergebnis.text = "Da kann ich leider nicht berechnen"; 
+        }
     }
 }
